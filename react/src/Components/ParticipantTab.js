149858874a3e5772d89c4b6d10e8fd81c73c1fdb
Berk Ayvalı <berkayvali@hotmail.com>
2022-07-21 16:51:59 +0300

    scroll added into participant screen.

--- react/src/Components/ParticipantTab.js
+++ react/src/Components/ParticipantTab.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Stack from '@mui/material/Stack';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import { SvgIcon } from './SvgIcon';
-import { SettingsContext, MediaSettingsContext } from 'pages/AntMedia';
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import { SvgIcon } from "./SvgIcon";
+import { SettingsContext, MediaSettingsContext } from "pages/AntMedia";
 
 const ParticipantName = styled(Typography)(({ theme }) => ({
-  color: '#ffffff',
+  color: "#ffffff",
   fontWeight: 500,
   fontSize: 14,
 }));
 
 const PinBtn = styled(Button)(({ theme }) => ({
-  '&:hover': {
+  "&:hover": {
     backgroundColor: theme.palette.green[50],
   },
 }));
@@ -23,20 +23,33 @@ function ParticipantTab(props) {
   const mediaSettings = React.useContext(MediaSettingsContext);
   const settings = React.useContext(SettingsContext);
 
-  const { pinnedVideoId, pinVideo,allParticipants } = settings;
+  const { pinnedVideoId, pinVideo, allParticipants } = settings;
   const getParticipantItem = (videoId, name) => {
     return (
-      <Grid key={videoId} container alignItems="center" justifyContent="space-between" style={{ borderBottomWidth: 1 }} sx={{ borderColor: 'primary.main' }}>
+      <Grid
+        key={videoId}
+        container
+        alignItems="center"
+        justifyContent="space-between"
+        style={{ borderBottomWidth: 1 }}
+        sx={{ borderColor: "primary.main" }}
+      >
         <Grid item sx={{ pr: 1 }}>
           <ParticipantName variant="body1">{name}</ParticipantName>
         </Grid>
         <Grid item>
           {pinnedVideoId === videoId ? (
-            <PinBtn sx={{ minWidth: 'unset', pt: 1, pb: 1 }} onClick={() => pinVideo(videoId)}>
+            <PinBtn
+              sx={{ minWidth: "unset", pt: 1, pb: 1 }}
+              onClick={() => pinVideo(videoId)}
+            >
               <SvgIcon size={28} name="unpin" color="#fff" />
             </PinBtn>
           ) : (
-            <PinBtn sx={{ minWidth: 'unset', pt: 1, pb: 1 }} onClick={() => pinVideo(videoId)}>
+            <PinBtn
+              sx={{ minWidth: "unset", pt: 1, pb: 1 }}
+              onClick={() => pinVideo(videoId)}
+            >
               <SvgIcon size={28} name="pin" color="#fff" />
             </PinBtn>
           )}
@@ -45,21 +58,24 @@ function ParticipantTab(props) {
     );
   };
   return (
-    <div style={{ width: '100%' }}>
-      <Stack sx={{ width: '100%' }} spacing={2}>
+    <div style={{ width: "100%" }}>
+      <Stack sx={{ width: "100%", overflowY: "auto" }} spacing={2}>
         <Grid container>
           <SvgIcon size={28} name="participants" color="#fff" />
-          <ParticipantName variant="body2" style={{ marginLeft: 4, fontWeight: 500 }}>
+          <ParticipantName
+            variant="body2"
+            style={{ marginLeft: 4, fontWeight: 500 }}
+          >
             {allParticipants.length + 1}
           </ParticipantName>
         </Grid>
-        {getParticipantItem('localVideo', 'You')}
+        {getParticipantItem("localVideo", "You")}
 
         {allParticipants.map(({ streamId, streamName }, index) => {
           if (mediaSettings?.myLocalData?.streamId !== streamId) {
             return getParticipantItem(streamId, streamName);
           } else {
-            return '';
+            return "";
           }
         })}
       </Stack>
