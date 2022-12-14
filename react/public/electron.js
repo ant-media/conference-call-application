d54a4253735b762e9632775d2f9f44b4eae81b21
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2022-12-15 00:05:58 +0300

    Implement nut-js

--- react/public/electron.js
+++ react/public/electron.js
@@ -1,7 +1,8 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 const path = require('path');
 const isDev = require('electron-is-dev');
+const {mouse, screen, right, left, up, down, straightTo, centerOf, Region, imageResource, sleep, Point} = require("@nut-tree/nut-js");
 
 let mainWindow = null;
 app.on('ready', createWindow);
@@ -15,11 +16,17 @@ app.on('activate', function () {
         createWindow()
     }
 });
+
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 1024,
-        title: "Ant Media Conference Application"
+        title: "Ant Media Conference Application",
+        webPreferences: {
+            nativeWindowOpen: true,
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     });
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.on('closed', function () {
@@ -28,4 +35,29 @@ function createWindow() {
     mainWindow.on('page-title-updated', function (e) {
         e.preventDefault()
     });
+    ipcMain.handle('move-mouse', (evt, arg) => {
+        console.log("Moving mouse!");
+        const square = async () => {
+            await mouse.move(right(500));
+            await mouse.move(down(500));
+            await mouse.move(left(500));
+            await mouse.move(up(500));
+        };
+
+        (async () => {
+            await square();
+            await mouse.move(straightTo(centerOf(new Region(100, 100, 200, 300))));
+        })();
+    });
+    ipcMain.handle('mouse-click', (evt, arg) => {
+        (async () => {
+            await mouse.setPosition(new Point(arg.x, arg.y));
+            if (arg.button === 'left') {
+                await mouse.leftClick();
+            } else if (arg.button === 'right') {
+                await mouse.rightClick();
+            }
+        })();
+    });
+
 }
