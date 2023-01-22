d17949a373015c3fd3845a5c1aacff3c01e4aa9a
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-01-22 16:12:09 +0300

    Update AntMedia.js

--- react/src/pages/AntMedia.js
+++ react/src/pages/AntMedia.js
@@ -210,7 +210,7 @@ function AntMedia() {
   }
 
   function getLang() {
-    if (navigator.languages != undefined)
+    if (navigator.languages !== undefined)
       return navigator.languages[0];
     return navigator.language;
   }
