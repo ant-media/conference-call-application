516587401e34cc8e33f44452f0ec8b21dfc22e9a
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-05-08 11:34:58 +0300

    Update AntMedia.js

--- react/src/pages/AntMedia.js
+++ react/src/pages/AntMedia.js
@@ -9,9 +9,9 @@ import { useSnackbar } from "notistack";
 import { SnackbarProvider } from "notistack";
 import AntSnackBar from "Components/AntSnackBar";
 import LeftTheRoom from "./LeftTheRoom";
-import {VideoEffect} from "@antmedia/webrtc_adaptor";
+import { VideoEffect } from "@antmedia/webrtc_adaptor/dist/video-effect";
 import { WebRTCAdaptor } from "@antmedia/webrtc_adaptor";
-import { getUrlParameter } from "@antmedia/webrtc_adaptor";
+import { getUrlParameter } from "@antmedia/webrtc_adaptor/dist/fetch.stream";
 import {SvgIcon} from "../Components/SvgIcon";
 import ParticipantListDrawer from "../Components/ParticipantListDrawer";
 
