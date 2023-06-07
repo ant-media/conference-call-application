65e7a7ec698dfd16ed6c70216b3b7b9eba352320
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-06-07 14:10:33 +0300

    Replace volume meter build with original one

--- createwar.sh
+++ createwar.sh
@@ -1,6 +1,21 @@
 cd react
 npm install
 npm run build
+
+# Whenever webpack modify volume-meter-processor.js, it adds some require statement at the beginning of the file and it breaks worklet.
+# To prevent this behaviour, we just replace file with the version come from node_modules
+
+volume_meter_unminified_js_path=$"./node_modules/@antmedia/webrtc_adaptor/dist/volume-meter-processor.js"
+volume_meter_js_path="$(find "./build/static/media" -name "volume-meter-processor*js" -type f -print -quit)"
+
+if [ -z "$volume_meter_js_path=" ]
+then
+      echo "\$volume_meter_js_path is empty passing.."
+else
+      rm $volume_meter_js_path
+      cp $volume_meter_unminified_js_path $volume_meter_js_path
+fi
+
 cd ..
 rm webapp/src/main/webapp/static/css/*
 rm webapp/src/main/webapp/static/js/*
