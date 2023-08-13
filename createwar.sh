f114e79cdcdae656e39e2f9d5b4e24db55275ca9
mekya <ahmetmermerkaya@gmail.com>
2023-08-13 16:22:30 +0300

    Dont include .env file because it breaks build in production

--- createwar.sh
+++ createwar.sh
@@ -1,9 +1,15 @@
 cd react
+#just move the .env file to .env.bak to not have any problems with the build
+mv .env .env.bak
 npm install
 npm run build
+#restore the .env file
+mv .env.bak .env 
 cd ..
 rm webapp/src/main/webapp/static/css/*
 rm webapp/src/main/webapp/static/js/*
 cp -a react/build/. webapp/src/main/webapp
+
+
 cd webapp
 mvn clean install -DskipTests -Dgpg.skip=true --quiet
