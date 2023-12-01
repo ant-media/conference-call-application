768735c16aa005c56d810f529b9c18d939157980
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-12-01 16:27:40 +0300

    Cleanup unused imports/variables

--- react/src/Components/MuteParticipantDialog.js
+++ react/src/Components/MuteParticipantDialog.js
@@ -4,18 +4,15 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 import {useContext} from "react";
 import {ConferenceContext} from "../pages/AntMedia";
-import {useTranslation} from "react-i18next";
 
 export default function MuteParticipantDialog() {
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
     const conference = useContext(ConferenceContext);
-    const { t } = useTranslation();
 
     const handleClose = () => {
         conference?.setMuteParticipantDialogOpen(false);
