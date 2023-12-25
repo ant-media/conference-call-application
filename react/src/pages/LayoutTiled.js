364d8f2d10eecfebea45b7246cf1910267482ac6
USAMAWIZARD <usamawizard@gmail.com>
2023-12-25 14:59:52 +0530

    fix clicking on options

--- react/src/pages/LayoutTiled.js
+++ react/src/pages/LayoutTiled.js
@@ -85,7 +85,7 @@ function LayoutTiled(props) {
           playingParticipants.map((element, index) => {
             var ismobilAndLocalvideo = element.id==="localVideo" && conference.globals.isMobileDevice;
             if(ismobilAndLocalvideo)
-              var mcardHeight = cardHeight +100 
+              var mcardHeight = cardHeight +200 
             //console.log("cw:"+cardWidth+" ch:"+cardHeight);
             return (
               <div
