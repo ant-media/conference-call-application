660e327de5fc19af0dcc1c64f39a3d69f0babf58
burak-58 <burak@antmedia.io>
2024-02-18 23:36:16 +0300

    edit jest config

--- react/jest.config.js
+++ react/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
         "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js"
    },
-   changedSince:'origin/main',
+   //changedSince:'origin/main',
    coverageThreshold: {
     global: {
       branches: 70,
