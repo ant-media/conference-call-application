434ce74cf341c3b7d4c9b8240d2304dfbcb92735
burak-58 <burak@antmedia.io>
2024-02-25 23:04:57 +0300

    Update browser.py

--- test/browser.py
+++ test/browser.py
@@ -26,13 +26,13 @@ class Browser:
     browser_options.add_argument('--disable-gpu')
     browser_options.add_argument('--disable-dev-shm-usage')
     browser_options.add_argument('--disable-setuid-sandbox')
-    #if is_headless:
-    #  browser_options.add_argument("--headless")
+    if is_headless:
+      browser_options.add_argument("--headless")
     
     dc = DesiredCapabilities.CHROME.copy()
     dc['goog:loggingPrefs'] = { 'browser':'ALL' }
-    service = Service(executable_path='C:/WebDriver/chromedriver.exe') 
-    #service = Service(executable_path='/tmp/chromedriver')
+    #service = Service(executable_path='C:/WebDriver/chromedriver.exe') 
+    service = Service(executable_path='/tmp/chromedriver')
     self.driver = webdriver.Chrome(service=service, options=browser_options)
 
   def open_in_new_tab(self, url):
