57247660ec315b4d8061acf1c6db02681b9b1479
burak-58 <burak@antmedia.io>
2024-05-29 10:41:10 +0300

    Update test_deployment.py

--- test/test_deployment.py
+++ test/test_deployment.py
@@ -12,18 +12,12 @@ class TestDeployment(unittest.TestCase):
     user = os.environ.get('AMS_USER')
     password = os.environ.get('AMS_PASSWORD')
     self.test_app_name = os.environ.get('TEST_APP_NAME')
+    self.war_file=os.environ.get('WAR_FILE')
     self.rest_helper = RestHelper(url, user, password)
     self.rest_helper.login()
     
   def test_install_app(self):
-    # burak: install app is currently not working through LB
-    # so we get first node ip and call install through it
-    nodes_json = self.rest_helper.call_get_cluster_nodes()
-    ip_of_first_node = nodes_json[0]["ip"]
-    temp_rest_helper = RestHelper("http://"+ip_of_first_node+":5080", self.rest_helper.user, self.rest_helper.password)
-    temp_rest_helper.login()
-
-    response = temp_rest_helper.call_install_app(os.environ.get('WAR_FILE'), self.test_app_name)
+    response = self.rest_helper.call_install_app(self.war_file, self.test_app_name)
     print(response)
     assert(response["success"])
     time.sleep(20)
