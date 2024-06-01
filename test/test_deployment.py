27dbbe8b301595540f2865c9ec8f4a4d6ed86683
burak-58 <burak@antmedia.io>
2024-06-01 12:11:46 +0300

    Update test_deployment.py

--- test/test_deployment.py
+++ test/test_deployment.py
@@ -29,7 +29,7 @@ class TestDeployment(unittest.TestCase):
     app_settings["stunServerURI"] = "turn:coturn"
     app_settings["turnServerUsername"] = "ovh36"
     app_settings["turnServerCredential"] = "ovh36"
-    print("App Settings before:" + app_settings)
+    print("App Settings before:" + str(app_settings))
     response = self.rest_helper.call_set_app_settings(self.test_app_name, app_settings)
     assert(response["success"])
 
