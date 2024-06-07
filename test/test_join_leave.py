067431e6cc9722682ab62b1b1b6606207082f026
burak-58 <burak@antmedia.io>
2024-06-07 22:43:46 +0300

    Update test_join_leave.py

--- test/test_join_leave.py
+++ test/test_join_leave.py
@@ -232,6 +232,14 @@ class TestJoinLeave(unittest.TestCase):
     print("switched to tab 1")
     self.chrome.print_ss_as_base64()
 
+    time.sleep(5)
+
+    print("----------------------")
+
+    self.chrome.print_console_logs()
+
+    print("----------------------")
+
 
     wait.until(lambda x: len(self.get_videoTrackAssignments()) == 1)
 
