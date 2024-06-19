d8a41e19fc4cbff57617a56dfc8a1834fb37a205
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2024-06-19 20:35:06 +0300

    Update WebSocketProvider.js

--- react/src/Components/WebSocketProvider.js
+++ react/src/Components/WebSocketProvider.js
@@ -70,11 +70,7 @@ export const WebSocketProvider = ({ children }) => {
 
             const pingInterval = setInterval(() => {
                 if (webSocket.current && webSocket.current.readyState === WebSocket.OPEN) {
-                    var jsCmd = {
-                      command: "ping"
-                    };
-
-                    webSocket.current.send(JSON.stringify(jsCmd));
+                    webSocket.current.send(JSON.stringify({command: "ping"}));
                 } else if (webSocket.current && webSocket.current.readyState === WebSocket.CLOSED) {
                     console.log('WebSocket not connected, unable to send ping');
                     webSocket.current = new WebSocket(applicationWebSocketUrl);
