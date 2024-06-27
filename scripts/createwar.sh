8f5677dc7a66eca00479e54371ad6966e140c90a
Jer <jeremy@librispark.com>
2024-06-26 20:10:06 -0400

    scripts

--- scripts/createwar.sh
+++ scripts/createwar.sh
@@ -10,9 +10,6 @@ echo "Checking for changes in the current branch..."
 check_for_changes
 echo "building circle for talentis"
 
-# Save the current branch name
-current_branch=$(git symbolic-ref --short HEAD)
-
 if [ "$(basename "$PWD")" != "scripts" ]; then
   cd ./scripts || { echo "Failed to change directory to ./scripts"; exit 1; }
 fi
@@ -33,22 +30,3 @@ cp -a react/build/. webapp/src/main/webapp
 
 cd webapp
 mvn clean install -DskipTests -Dgpg.skip=true --quiet
-### original createwar.sh above (cupy upstream updates above) ###
-
-# cleanup
-sleep 2
-
-# Reset back to the original branch state including untracked files
-git reset --hard HEAD
-
-# Clean untracked files
-git clean -fd
-git clean -fdx
-
-# Ensure the cleanup by forcing the refresh of the working directory
-git status > /dev/null
-git clean -fd
-git clean -fdx
-
-# Checkout the original branch
-git checkout "$current_branch"
