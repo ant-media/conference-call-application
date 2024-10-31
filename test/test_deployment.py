a0a980acbcc59611424e5bbc5921a2a6075b10b6
burak-58 <burak@antmedia.io>
2024-10-31 18:10:53 +0300

    uncomment delete app test

--- test/test_deployment.py
+++ test/test_deployment.py
@@ -64,6 +64,7 @@ class TestDeployment(unittest.TestCase):
     response = self.rest_helper.call_delete_app(self.test_app_name)
     print(response)
     assert(response["success"])
+    time.sleep(60)
 
 if __name__ == '__main__':
     unittest.main()
