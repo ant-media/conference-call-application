e4eaabf404f73903b8a323a887d5fcd5e24182c4
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2025-01-02 15:17:19 +0300

    save settings as state

--- react/src/pages/AntMedia.js
+++ react/src/pages/AntMedia.js
@@ -338,6 +338,8 @@ function AntMedia(props) {
     const [microphoneButtonDisabled, setMicrophoneButtonDisabled] = React.useState(false);
     const [cameraButtonDisabled, setCameraButtonDisabled] = React.useState(false);
 
+    const [settings, setSettings] = React.useState();
+
     const [screenSharingInProgress, setScreenSharingInProgress] = React.useState(false);
 
   const [requestSpeakerList, setRequestSpeakerList] = React.useState([]);
@@ -2964,6 +2966,7 @@ function AntMedia(props) {
                 console.log("--maxVideoTrackCountFromAppSettings: ", localSettings?.maxVideoTrackCount);
                 setAppSettingsMaxVideoTrackCount(localSettings?.maxVideoTrackCount > 0 ? localSettings?.maxVideoTrackCount+1 : 6);
             }
+            setSettings(localSettings);
         } else if (obj.command === "startRecordingResponse") {
             console.log("Incoming startRecordingResponse:", obj);
             definition = JSON.parse(obj.definition);
@@ -3172,7 +3175,8 @@ function AntMedia(props) {
                             processUpdatedStatsForPlaySpeedTest,
                             speedTestCounter,
                             setRoomName,
-                            setPublishStreamId
+                            setPublishStreamId,
+                            settings
                         }}
                     >
                         {props.children}
