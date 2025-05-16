17a4c6b2dca0c4c7dece86fc540a8c572cc5cdac
Jer <jeremy@librispark.com>
2025-05-16 12:17:18 -0400

    update createwar

--- scripts/createwar.sh
+++ scripts/createwar.sh
@@ -19,28 +19,12 @@ cp -r ./build_replace/* ../
 cd ..
 
 ### original createwar.sh below (cupy upstream updates below) ###
-# cd react
-# npm install
-# npm run build
-# cd ..
-# rm webapp/src/main/webapp/static/css/*
-# rm webapp/src/main/webapp/static/js/*
-# cp -a react/build/. webapp/src/main/webapp
-
-
-# cd webapp
-# mvn clean install -DskipTests -Dgpg.skip=true --quiet
 cd react
-npm install --legacy-peer-deps
+npm install
 npm run build
 cd ..
-if [ -d "webapp/src/main/webapp/static" ] && [ "$(ls -A webapp/src/main/webapp/static)" ]; then
-    echo "Directory webapp/src/main/webapp/static/ exists and has files. Removing files..."
-    rm -rf webapp/src/main/webapp/static/*
-    echo "All files in webapp/src/main/webapp/static/ have been removed."
-else
-    echo "Directory webapp/src/main/webapp/static/ is either empty or does not exist. No files to remove."
-fi
+rm webapp/src/main/webapp/static/css/*
+rm webapp/src/main/webapp/static/js/*
 cp -a react/build/. webapp/src/main/webapp
 
 
