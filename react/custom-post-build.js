f3f2e6f88e48e8f949a7f6012df00cad270bab90
golgetahir <tahir.golge@gmail.com>
2025-08-11 10:53:29 +0300

    Add Portoguese language support to circle

--- react/custom-post-build.js
+++ react/custom-post-build.js
@@ -4,13 +4,20 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
+const buildDir = process.env.BUILD_PATH ? process.env.BUILD_PATH : './build';
 const volumeMeterUnminifiedJsPath = './node_modules/@antmedia/webrtc_adaptor/dist/volume-meter-processor.js';
-const volumeMeterJsPath = glob.sync('./build/static/media/volume-meter-processor*.js', { nodir: true })[0];
+const pattern = path.join(buildDir, 'static/media/volume-meter-processor*.js');
+const matches = glob.sync(pattern, { nodir: true });
+const volumeMeterJsPath = matches && matches.length > 0 ? matches[0] : null;
 
 if (!volumeMeterJsPath) {
   console.log('$volumeMeterJsPath is empty. Skipping...');
 } else {
-  fs.unlinkSync(volumeMeterJsPath);
+  try {
+    fs.unlinkSync(volumeMeterJsPath);
+  } catch (e) {
+    console.log('Could not unlink existing volume-meter-processor file, will overwrite:', e.message);
+  }
   fs.copyFileSync(volumeMeterUnminifiedJsPath, volumeMeterJsPath);
 }
 
